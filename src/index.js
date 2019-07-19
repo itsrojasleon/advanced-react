8185da4d1a53939197aff9a116f3560b57855fb2
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-07-19 17:04:48 -0500

    Configure now

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
-  return <div>Hello</div>;
+  return <h1 style={{ color: 'lightsalmon' }}>Hello</h1>;
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
