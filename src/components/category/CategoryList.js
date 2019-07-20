c285630624d2d4bb027f232e002b7c84d278e236
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-07-20 18:43:48 -0500

    add useEffect and useState hooks

--- src/components/category/CategoryList.js
+++ src/components/category/CategoryList.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Category from './Category';
 import { List, Item } from '../../styles/category/category-list';
-import { categories } from '../../../api/db.json';
 
 export default function CategoryList() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    let ignore = false;
+
+    async function fetchData() {
+      const req = await fetch(
+        `https://petgram-app-server-ln46kf2xq.now.sh/categories`,
+      );
+      const res = await req.json();
+      if (!ignore) setCategories(res);
+    }
+
+    fetchData();
+    return () => {
+      ignore = true;
+    };
+  }, []);
   return (
     <List>
       {categories.map(category => (
