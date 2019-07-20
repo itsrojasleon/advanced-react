a4a5d03db70479ca2bec2dc9cc375e11b124b8df
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-07-19 21:00:53 -0500

    Adding a svg logo

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import CategoryList from './category/CategoryList';
 import PhotoCardList from './photo-card/PhotoCardList';
+import Logo from './Logo';
 import { GlobalStyle } from '../global-style';
 
 export default function App() {
   return (
     <div>
       <GlobalStyle />
+      <Logo />
       <CategoryList />
       <PhotoCardList />
     </div>
