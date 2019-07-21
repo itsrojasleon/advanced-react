3c6ad1da2e1fc7c6cd5a6bf8a83f0de58c83b2e2
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-07-21 15:45:01 -0500

    Add my custom hook

---
+++ src/hooks/useFetchResource.js
@@ -0,0 +1,38 @@
+// The functionality of this Hook is fetch any resource inside the API
+// The API has many endpoints, we can use them for get any data
+
+import { useState, useEffect } from 'react';
+
+export default function useFetchResource(resource) {
+  const [resources, setResources] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    let BASE_URL = 'https://petgram-app-server-ln46kf2xq.now.sh';
+    let ignore = false;
+    setLoading(true);
+
+    async function fetchData() {
+      try {
+        const request = await fetch(`${BASE_URL}/${resource}`);
+        const response = await request.json();
+        if (!ignore) {
+          setResources(response);
+          setLoading(true);
+        }
+      } catch (e) {
+        setLoading(false);
+        setError(e.message);
+      }
+    }
+
+    fetchData();
+
+    return () => {
+      ignore = true;
+    };
+  }, [resource]);
+
+  return [resources, loading, error];
+}
