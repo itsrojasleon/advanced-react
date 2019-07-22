253718a957f54d3c075ba38e6997be176731a60a
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-07-22 14:19:32 -0500

    Polyfill

--- src/components/photo-card/PhotoCard.js
+++ src/components/photo-card/PhotoCard.js
@@ -6,23 +6,23 @@ const DEFAULT_IMAGE =
   'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60';
 
 export default function PhotoCard({ id, likes = 0, src = DEFAULT_IMAGE }) {
-  const [show, setShow] = useState();
+  const [show, setShow] = useState(false);
   const el = useRef();
 
-  useEffect(
-    function() {
-      const observer = new window.IntersectionObserver(function(entries) {
-        const { isIntersecting } = entries[0];
-        console.log(isIntersecting);
-        if (isIntersecting) {
-          setShow(true);
-          observer.disconnect();
-        }
-      });
-      observer.observe(el.current);
-    },
-    [el],
-  );
+  useEffect(() => {
+    import('intersection-observer');
+    const observer = new window.IntersectionObserver(function(entries) {
+      const { isIntersecting } = entries[0];
+      if (isIntersecting) {
+        setShow(true);
+        observer.disconnect();
+      }
+    });
+    observer.observe(el.current);
+    () => {
+      observer.unobserve(el.current);
+    };
+  }, [el]);
   return (
     <Article ref={el}>
       {show && (
